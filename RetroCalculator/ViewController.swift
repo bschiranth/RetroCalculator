5447bb284b80dae707d78786c02270de36a4e7ca
bschiranth <bschiranth@gmail.com>
2016-08-22 03:24:46 -0400

    First
    - added clear button and intro screen

--- RetroCalculator/ViewController.swift
+++ RetroCalculator/ViewController.swift
@@ -7,19 +7,116 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var runningNumber = ""
+    
+    var currentOperation = Operation.empty
+    
+    var leftValStr = ""
+    var rightValStr = ""
+    var result = ""
+    
+    var btnSound:AVAudioPlayer!
+    
+    enum Operation:String {
+        case Add = "+"
+        case Subtract = "-"
+        case Divide = "/"
+        case Multiply = "*"
+        case empty = "empty"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.pathForResource("btn", ofType:"wav") // path to file which is in main bundle
+        let soundURL = URL(fileURLWithPath: path!) // initialize URL with file path
+        
+        do { //like try catch block
+            try btnSound = AVAudioPlayer(contentsOf: soundURL) // Audioplayer with contents of given URL
+            btnSound.prepareToPlay() //get ready to play
+        } catch let err as NSError {
+            print(err.debugDescription)         }
+            }
+    
+    //this action is applicable to each number
+    @IBAction func numberPressed(sender:UIButton){
+        playSound()
+        
+        runningNumber += "\(sender.tag)" // append the tag of clicked number to this string
+        outputLabel.text = runningNumber // then assign it to output label
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onDividePressed(sender:AnyObject){
+        processOperation(operation: Operation.Divide)
     }
-
-
+    @IBAction func onMultiplyPressed(sender:AnyObject){
+        processOperation(operation: Operation.Multiply)
+    }
+    @IBAction func onSubtractPressed(sender:AnyObject){
+        processOperation(operation: Operation.Subtract)
+    }
+    @IBAction func onAddPressed(sender:AnyObject){
+        processOperation(operation: Operation.Add)
+    }
+    @IBAction func onEqualPressed(sender:AnyObject){
+        processOperation(operation: currentOperation)
+    }
+    
+    
+    @IBAction func clearPressed(_ sender: AnyObject) {
+        playSound()
+        outputLabel.text = "0"
+        runningNumber = ""
+    }
+    
+    func playSound(){
+        if(btnSound.isPlaying) {
+            btnSound.stop()
+        }
+        btnSound.play()
+    }
+    
+    //func accepts only operation which is of type Operation
+    func processOperation(operation:Operation){
+        
+        playSound()
+        
+        if(currentOperation != Operation.empty){
+            if(runningNumber != ""){
+                rightValStr = runningNumber
+                runningNumber = ""
+                
+                if(currentOperation == Operation.Multiply){
+                    result = "\(Double(leftValStr)! * Double(rightValStr)!)"
+                    
+                }
+                else if(currentOperation == Operation.Divide){
+                 result = "\(Double(leftValStr)! / Double(rightValStr)!)"
+                }
+                else if(currentOperation == Operation.Add){
+                     result = "\(Double(leftValStr)! + Double(rightValStr)!)"
+                }
+                else if(currentOperation == Operation.Subtract){
+                     result = "\(Double(leftValStr)! - Double(rightValStr)!)"
+                }
+                
+                leftValStr = result
+                outputLabel.text = result
+            }
+            currentOperation = operation
+        }else{
+            //Mark:operator pressed for first time
+            leftValStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+    }
 }
 
